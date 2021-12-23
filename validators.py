d2f2b342fbea03e4a02f86e021edcca086ae21d1
Kouff <thematuhaimmortal@gmail.com>
2021-12-23 11:48:47 +0200

    improve the validator for required fields and add a new validator for unrequired fields

--- validators.py
+++ validators.py
@@ -3,10 +3,10 @@ from functools import wraps
 from flask import request, jsonify
 
 
-def validate_field(fields: dict):
-    def validate_field_(func):
+def validate_required_fields(fields: dict):
+    def validate_required_fields_(func):
         @wraps(func)
-        def wrapped():
+        def wrapped(*args, **kwargs):
             error_required_fields = []
             error_value_fields = []
             data = request.json or {}
@@ -18,14 +18,36 @@ def validate_field(fields: dict):
                     error_value_fields.append(field)
                     continue
             if error_required_fields or error_value_fields:
-                data = {}
+                error_data = {}
                 for field in error_required_fields:
-                    data[field] = 'This field is requirement.'
+                    error_data[field] = 'This field is requirement.'
                 for field in error_value_fields:
-                    data[field] = f'The value must be {fields[field]}.'
-                return jsonify(data), 400
-            return func()
+                    error_data[field] = f'The value must be {fields[field]}.'
+                return jsonify(msgs=error_data), 400
+            return func(*args, **kwargs)
 
         return wrapped
 
-    return validate_field_
+    return validate_required_fields_
+
+
+def validate_fields(fields: dict):
+    def validate_fields_(func):
+        @wraps(func)
+        def wrapped(*args, **kwargs):
+            error_value_fields = []
+            data = request.json or {}
+            for field, types in fields.items():
+                if field in data and not isinstance(data[field], types):
+                    error_value_fields.append(field)
+                    continue
+            if error_value_fields:
+                error_data = {}
+                for field in error_value_fields:
+                    error_data[field] = f'The value must be {fields[field]}.'
+                return jsonify(msgs=error_data), 400
+            return func(*args, **kwargs)
+
+        return wrapped
+
+    return validate_fields_
